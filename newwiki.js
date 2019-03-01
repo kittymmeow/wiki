7f512d0c828aeb8050885bbb3efcd86e41faeab5
kittymmeow <emameow@hotmail.com>
2019-03-01 13:49:15 -1000

    Create newwiki.js

---
+++ newwiki.js
@@ -0,0 +1,121 @@
+var action = {
+    protect: [{
+            page: "Template:Publisher",
+            summary: "Standard practice"
+        }, {
+            page: "Template:Developer",
+            summary: "Standard practice"
+        },
+        {
+            page: "File:Favicon.ico",
+            summary: "Used as part of wiki skin"
+        },
+        {
+            page: "File:Wiki.png",
+            summary: "Used as part of wiki skin"
+        },
+        {
+            page: "Project:About",
+            summary: "Standard practice"
+        },
+        {
+            page: "Project:Copyrights",
+            summary: "Standard practice"
+        },
+        {
+            page: "Project:General disclaimer",
+            summary: "Standard practice"
+        },
+        {
+            page: "Template:Admins",
+            summary: "Standard practice"
+        },
+        {
+            page: "Template:FP link",
+            summary: "Standard practice"
+        },
+        {
+            page: "Template:FP header",
+            summary: "Standard practice"
+        },
+        {
+            page: "Template:Wiki admin",
+            summary: "Standard practice"
+        },
+        {
+            page: "Template:Featured images",
+            summary: "Standard practice"
+        },
+        {
+            page: gamename + "Wiki",
+            summary: "Standard practice"
+        },
+        {
+            page: gamename + "Wiki/Section 1",
+            summary: "Standard practice"
+        },
+        {
+            page: gamename + "Wiki/Section 2",
+            summary: "Standard practice"
+        },
+        {
+            page: gamename + "Wiki/Section 3",
+            summary: "Standard practice"
+        },
+        {
+            page: gamename + "Wiki/Section 4",
+            summary: "Standard practice"
+        }
+    ],
+    delete: [{
+            page: "MediaWiki:2colmainpage.css",
+            summary: ""
+        },
+        {
+            page: "MediaWiki:2colmainpage.js",
+            summary: ""
+        },
+        {
+            page: "MediaWiki:3colmainpage.css",
+            summary: ""
+        },
+        {
+            page: "MediaWiki:3colmainpage.js",
+            summary: ""
+        },
+        {
+            page: "MediaWiki:Dark mobile.css",
+            summary: ""
+        },
+        {
+            page: "MediaWiki:Light mobile.css",
+            summary: ""
+        },
+        {
+            page: gamename + "Wiki/Top section",
+            summary: ""
+        },
+        {
+            page: gamename + "Wiki/Flex section",
+            summary: ""
+        },
+        {
+            page: gamename + "Wiki/Bottom section",
+            summary: ""
+        },
+        {
+            page: "2 column responsive main page (alternative)",
+            summary: ""
+        }
+    ],
+    move: [{
+        page: "How to play guide for Default Loadout",
+        destination: "How to play guide for" + gamename;
+    }],
+	watch: [{
+		page:"Project:Community portal"
+	},
+	{
+		page:"Project:Admin noticeboard"
+	}]
+};
